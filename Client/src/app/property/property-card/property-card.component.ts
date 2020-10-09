bfaed498846e5d8b229377aa37a2b705e3de10da
Sevdalin Vasilev <predator187@abv.bg>
2020-10-09 15:09:35 +0300

    Add properties list and css styles to it

--- Client/src/app/property/property-card/property-card.component.ts
+++ Client/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -9,10 +9,5 @@ styleUrls: ['property-card.component.css']
 )
 
 export class PropertyCardComponent{
-Property: any={
-  "Id":1,
-  "Name":"Birla house",
-  "Type":"House",
-  "Price":34000
-}
+@Input() property : any
 }
--- Client/src/app/property/property-list/property-list.component.ts
+++ Client/src/app/property/property-list/property-list.component.ts
@@ -6,6 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  properties : Array<any>=[
+    {
+    "Id":1,
+    "Name":"Birla house",
+    "Type":"House",
+    "Price":34000
+    },
+    {
+      "Id":2,
+      "Name":"Montreal",
+      "Type":"House",
+      "Price":55000
+    },
+    {
+        "Id":3,
+        "Name":"Macro Home",
+        "Type":"House",
+        "Price":25000
+    },
+    {
+      "Id":4,
+      "Name":"Sunrise Mansion",
+      "Type":"Mansion",
+      "Price":150000
+    }
+  ]
 
   constructor() { }
 
