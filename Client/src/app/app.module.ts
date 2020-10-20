d570ee44a875b1770746ea32b8d3ded359f87b5a
Sevdalin Vasilev <predator187@abv.bg>
2020-10-20 17:13:26 +0300

    Add property details edit

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
 
 const appRoutes: Routes= [
   { path: '', component: PropertyListComponent},
   { path: 'rent-property', component: PropertyListComponent},
+  { path: 'property-detail/:id', component: PropertyDetailComponent},
   { path: 'add-property', component: AddPropertyComponent}
 ]
 
@@ -21,6 +23,7 @@ const appRoutes: Routes= [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
+    PropertyDetailComponent,
     NavBarComponent,
     AddPropertyComponent
    ],
--- Client/src/app/property/property-card/property-card.component.ts
+++ Client/src/app/property/property-card/property-card.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Input } from '@angular/core';
 import { IProperty } from '../IProperty.interface';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
-selector: 'app-property-card',
-templateUrl: 'property-card.component.html',
-styleUrls: ['property-card.component.css']
-}
-)
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.scss']
+})
 
 export class PropertyCardComponent{
-@Input() property : IProperty
-}
+  @Input() property : IProperty
+  }
