78e9532d84c8f1a97fd773945726394d1311388d
Sevdalin Vasilev <predator187@abv.bg>
2020-11-05 18:24:42 +0200

    Add reactive form to add property

--- Client/src/app/property/property-list/property-list.component.ts
+++ Client/src/app/property/property-list/property-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
-import { IProperty } from '../../model/IProperty';
+import { IPropertyBase } from '../../model/IPropertyBase';
 
 
 @Component({
@@ -11,7 +11,7 @@ import { IProperty } from '../../model/IProperty';
 })
 export class PropertyListComponent implements OnInit {
   SellRent = 1;
-  properties : Array<IProperty>;
+  properties : Array<IPropertyBase>;
 
   constructor(private route: ActivatedRoute, private housingService : HousingService) { }
 
