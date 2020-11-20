18ab0477c84fdcd028c181e4b616950790b357ac
Sevdalin Vasilev <predator187@abv.bg>
2020-11-20 18:52:31 +0200

    Add city add/delete logic

--- Server/WebAPI/Controllers/CityController.cs
+++ Server/WebAPI/Controllers/CityController.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using WebAPI.Data;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace WebAPI.Controllers
+﻿namespace WebAPI.Controllers
 {
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using WebAPI.Models;
+
     [Route("api/[controller]")]
     public class CityController : Controller
     {
@@ -17,37 +18,33 @@ namespace WebAPI.Controllers
             this.dbContext = dbContext;
         }
 
-        // GET: api/<controller>
         [HttpGet]
-        public IActionResult GetAllCities()
+        public async Task<IActionResult> GetAllCities()
         {
-            var cities= this.dbContext.Cities.ToList();
+            var cities = await this.dbContext.Cities.ToListAsync();
             return this.Ok(cities);
         }
 
-        // GET api/<controller>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpPost("add")]
+        [HttpPost("add/{cityName}")]
+
+        public async Task<IActionResult> AddCity(string cityName)
         {
-            return "value";
+            City city = new City();
+            city.Name = cityName;
+           await this.dbContext.AddAsync(city);
+           await this.dbContext.SaveChangesAsync();
+            return this.Ok(city);
         }
 
-        // POST api/<controller>
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
+        [HttpDelete("delete/{cityId}")]
 
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> DeleteCity(int cityId)
         {
-        }
-
-        // DELETE api/<controller>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var city = await this.dbContext.Cities.FindAsync(cityId);
+            this.dbContext.Remove(city);
+            await this.dbContext.SaveChangesAsync();
+            return this.Ok(cityId);
         }
     }
 }
